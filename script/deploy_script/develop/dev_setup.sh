fa255022f7b68e6b4bcf14a39302364bdde01f27
xiaoyu <xiaoyu@qq.com>
2025-07-23 23:18:20 +0800

    添加develop环境中的setup脚本

--- script/deploy_script/develop/dev_setup.sh
+++ script/deploy_script/develop/dev_setup.sh
@@ -13,15 +13,11 @@ source ${proxy_dir}
 open_shell_proxy
 open_git_proxy
 open_npm_proxy
-# 配置系统最大监控文件数
-sudo sysctl fs.inotify.max_user_watches=524288
 # 确保 Ghost 目录存在
 if [ ! -d "$ghost_dir" ]; then
     echo "Ghost 源代码目录 '$ghost_dir' 不存在，请检查配置。"
     exit 1
 fi
-# 赋予最高权限
-sudo chmod -R 777 $ghost_dir
 cd $ghost_dir
 # 启动ghost开发环境
-yarn dev
+yarn setup
